c8c4711b5ee87298e6a85391b6df99c9ed384955
ayush24k <ayushanshu69@gmail.com>
2025-01-17 23:43:55 +0530

    Rant Post Section

--- src/app/_components/ButtonComp.tsx
+++ src/app/_components/ButtonComp.tsx
@@ -8,7 +8,11 @@ type ButtonProp = {
 
 
 export default function ButtonComp({small = false, gray= false, className="" ,...props}:ButtonProp) {
+
+    const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold";
+    const colorClasses = gray ? "bg-gray-400 hover:bg-gray-300 focus-visible:bg-gray-300" : "bg-green-500 hover:bg-green-400 focus-visible:bg-green-400";
+
     return (
-        <button {...props}></button>
+        <button className={` rounded-full transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 text-white ${sizeClasses} ${colorClasses} ${className}`}  {...props}></button>
     )
 }
--- src/app/_components/RantPost.tsx
+++ src/app/_components/RantPost.tsx
@@ -1,13 +1,20 @@
+'use client'
+import { useSession } from "next-auth/react";
 import ButtonComp from "./ButtonComp";
+import ProfileImage from "./ProfileImage";
 
 export default function RantPost () {
+    const session = useSession();
+    
+    if (session.status !== "authenticated") return;
+
     return (
         <form className="flex flex-col gap-2 border-b-2 px-4 py-3">
             <div className="flex gap-4">
-                {/* <ProfileImage></ProfileImage> */}
+                <ProfileImage src={session.data.user.image} />
                 <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="rant here"></textarea>
             </div>
-            <ButtonComp>Raaant</ButtonComp>
+            <ButtonComp className="self-end">Raaant</ButtonComp>
         </form>
     )
 }
